352fcce08b3d2eb86d291071fd61ed4d0ad17ccd
JegenThierry <jegenthierry@gmail.com>
2026-03-17 19:50:38 +0100

    chore: Updates dependencies and defaults for templates

--- budget-planner-ui/nuxt.config.ts
+++ budget-planner-ui/nuxt.config.ts
@@ -4,9 +4,16 @@ export default defineNuxtConfig({
     devtools: {enabled: false},
     modules: ['@nuxt/ui', '@pinia/nuxt'],
     css: ['@/assets/css/main.css'],
+    vite: {
+        optimizeDeps: {
+            include: [
+                'axios'
+            ]
+        }
+    },
     runtimeConfig: {
         public: {
-            apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000/api",
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8080/api",
         }
     }
 })
