43b497000206d746696a2efe86bba169accd0ad3
JegenThierry <jegenthierry@gmail.com>
2026-07-14 18:54:17 +0200

    chore(release): bump version to 0.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ val guavaVersion = libs.versions.guava.get()
 
 allprojects {
     group = "beer.thierry"
-    version = "0.6.1"
+    version = "0.7.0"
 
     repositories {
         mavenCentral()
