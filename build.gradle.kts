10313ee49797dca57f33b1356f3f22d92f87caa2
JegenThierry <jegenthierry@gmail.com>
2026-07-18 22:13:30 +0200

    chore(release): bump version to 0.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ val guavaVersion = libs.versions.guava.get()
 
 allprojects {
     group = "beer.thierry"
-    version = "0.8.1"
+    version = "0.8.2"
 
     repositories {
         mavenCentral()
